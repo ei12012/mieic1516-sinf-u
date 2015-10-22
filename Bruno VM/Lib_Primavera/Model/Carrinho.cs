1840ee7e17de2450d2d0405d4700c91ddb92d3f9
p.carreira1 <p.carreira@outlook.pt>
2015-10-23 00:33:35 +0100

    Serviços do carrinho -  listar e remover produtos

---
+++ Bruno VM/Lib_Primavera/Model/Carrinho.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class Carrinho
+    {
+        public string cliente { get; set; }
+        public string artigo { get; set; }
+        public int plataforma { get; set; }
+        public double preco { get; set; }
+        public int stock { get; set; }
+        public int vendidos { get; set; }
+    }
+}
