1b300c6b252aa234ef01ab14ed1d70397c997ce8
moreira <ei12012@fe.up.pt>
2015-10-28 20:17:59 +0000

    WS_Pesquisar

---
+++ VMs/Bruno VM/Controllers/PesquisaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstREST.Lib_Primavera.Model;
+
+namespace FirstREST.Controllers
+{
+    public class PesquisaController : ApiController
+    {
+        // GET /api/acesso?email=ei12012@fe.up.pt&password=pass
+        public List<Lib_Primavera.Model.Artigo> Get(string tipo, string arg)
+        {
+            List<Lib_Primavera.Model.Artigo> artigo;
+
+            if (tipo == "plataforma")
+            {
+                artigo = Lib_Primavera.Integration.IntegracaoPesquisa.GetPlataforma(arg);
+            }
+            else if (tipo == "nome")
+            {
+                artigo = Lib_Primavera.Integration.IntegracaoPesquisa.GetNome(arg);
+            }
+            else if (tipo == "empresa")
+            {
+                artigo = Lib_Primavera.Integration.IntegracaoPesquisa.GetEmpresa(arg);
+            }
+            else
+            {
+                return null;
+            }
+            return artigo;
+        }
+    }
+}
