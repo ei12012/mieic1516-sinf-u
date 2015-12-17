cfdbfc2c514e183420bdb83f5d761af7e4d25a2d
moreira <ei12012@fe.up.pt>
2015-12-17 02:19:57 +0000

    Mudar página inicial

--- Webpage/index.php
+++ Webpage/index.php
@@ -7,14 +7,36 @@ $nome_pagina[1] = $_SERVER["REQUEST_URI"];
 include_once("config/init.php");
 
 // ACESSO A BASE DE DADOS
-include_once($BASE_DIR . "database/jogos.php");
+//include_once($BASE_DIR . "database/jogos.php");
+include_once($BASE_DIR . "database/primavera/artigos.php");
 
 // TRATAMENTO DE INFORMACAO
-$jogosVisitados = obterJogosMaisVisitados();
-$jogosRecentes = obterJogosMaisRecentes();
+$jogosVisitados = obterArtigosVisitados();
+$jogosRecentes = obterArtigosRecentes();
 
 // INCLUIR HEADER
 include_once($BASE_DIR . "pages/template/begin.php");
+
+function apresentar($arg)
+{
+	global $BASE_URL;
+	echo "<div class='col-sm-4'>";
+		echo "<div class='panel panel-default'>";
+			echo "<ul class='list-group'>";
+				echo "<li class='list-group-item'>";
+					echo "<img src=" . $BASE_URL . "images/capas/" . $arg->{'CodArtigo'} . ".png class='img-thumbnail' alt='jogo' width='360' height='240'/>";
+					echo "<p></p>";
+					echo "<h4 class='list-group-item-heading'>";
+						echo "<a href=" . $BASE_URL . "pages/jogo.php?id=" . $arg->{'CodArtigo'} . ">";	
+							echo $arg->{'Descricao'};
+						echo "</a>";			
+					echo "</h4>";
+					echo "<p class='list-group-item-text'>" . round($arg->{'Preco'}, 2) . "€</p>";				
+				echo "</li>";	
+			echo "</ul>";	
+		echo "</div>";
+	echo "</div>";	
+}
 ?>
 
 <div class="container">
@@ -23,73 +45,26 @@ include_once($BASE_DIR . "pages/template/begin.php");
 </div>
 
 <div class="container">
-	<div class="row">
-		<div class="col-sm-4">
-		   <div class="panel panel-default">
-		      <div class="panel-heading">
-		         Jogos + Visitados
-		      </div>
-
-		      <ul class="list-group">
-		      	<?php
-						foreach($jogosVisitados as $jogo)
-						{
-							?>
-								<li class="list-group-item">
-									<a href="<?php echo $BASE_URL;?>pages/jogo.php?id=<?php echo $jogo['id'];?>"><?php echo $jogo["nome"]; ?></a>
-									<p><?php echo $jogo["visitas"]; ?> Visitas</p>
-								</li>
-							<?php
-						}
-		      	?>
-		      </ul>
-		   </div>
-		</div>
-
-		<div class="col-sm-4">
-		   <div class="panel panel-default">
-		      <div class="panel-heading">
-		         Jogos + Recentes
-		      </div>
-
-		      <ul class="list-group">
-		      	<?php
-						foreach($jogosRecentes as $jogo)
-						{
-							?>
-								<li class="list-group-item">
-									<a href="<?php echo $BASE_URL;?>pages/jogo.php?id=<?php echo $jogo['id'];?>"><?php echo $jogo["nome"]; ?></a>
-									<p>Ano <?php echo $jogo["ano"]; ?> </p>
-								</li>
-							<?php
-						}
-		      	?>
-		      </ul>
-		   </div>
-		</div>
-
-		<div class="col-sm-4">
-		   <div class="panel panel-default">
-		      <div class="panel-heading">
-		         Jogos + Vendidos
-		      </div>
-
-		      <ul class="list-group">
-		      	<li class="list-group-item">
-						<a href="<?php echo $BASE_URL;?>pages/jogo.php?id=1">Grand Theft Auto V</a>
-						<p>13 Unidades</p>
-					</li>
-					<li class="list-group-item">
-						<a href="<?php echo $BASE_URL;?>pages/jogo.php?id=2">Final Fantasy XIV: A Realm Reborn</a>
-						<p>12 Unidades</p>
-					</li>
-		      </ul>
-		   </div>
-		</div>
-	</div>
+	<h2 class="text-primary">Visitados</h2>
+	<?php
+		for($index = 0; $index < count($jogosVisitados) && $index < 3; $index++)
+		{
+			$artigo = $jogosVisitados[$index];
+			apresentar($artigo);
+		}
+	?>
 </div>
 
-
+<div class="container">
+	<h2 class="text-primary">Recentes</h2>
+	<?php
+		for($index = 0; $index < count($jogosRecentes) && $index < 3; $index++)
+		{
+			$artigo = $jogosRecentes[$index];
+			apresentar($artigo);
+		}
+	?>
+</div>
 
 <?php
 include_once($BASE_DIR . "pages/template/end.php");
